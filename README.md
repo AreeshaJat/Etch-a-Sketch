385c68f1e9a015bc2dd8f6f642b524b260b9b144
AreeshaJat <areesha.jat2715@gmail.com>
2023-12-22 17:29:49 -0500

    Added functionality to the grid

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Etch-a-Sketch
+
+## Project Description
+This is a Etch-a-Sketch grid where the user is able to adjust the grid size, use colors such as black, grey and rainbow, erase the grid 
+and clear the grid.
+
+## Features and Functionality
+The web page consists of 6 buttons. Starting off with the prompt button. The prompt button is essential as it asks the user to input a grid size. Without the grid size, the grid is not created and the user is unable to use the other buttons. Once the user enters a grid size, there are 5 buttons they can select from. This consists 3 colors being black, grey and rainbow and then an eraser and a clear button. The user is able to accessorize their sketch based on these button.
+
+## Technologies Used
+- JavaScript
+- HTML
+- CSS
+
+The project is based on the development of a responsive and interactive web page using JavaScript, HTML and CSS.
+
+## How to Use
+1. Clone this repository to your local machine.
+2. Open the project folder in your code editor.
+3. View the HTML file to see the webpage structure.
+4. View the JavaScript file to explore the functionality.
+5. Explore the CSS files to understand the styling and layout.
+
+## Viewing the Webpage
+To view the completed webpage, follow this link: [Live Demo]()
--- script.js
+++ script.js
@@ -1,6 +1,44 @@
 //target location to generate the grid
 let container = document.querySelector('.container');
 container.style.cssText = "background-color: white;"
+
+//buttons
+const blackButton = document.getElementById('black');
+const greyButton = document.getElementById('grey');
+const rainbowButton = document.getElementById('rainbow');
+const eraserButton = document.getElementById('eraser');
+const clearButton = document.getElementById('clear');
+const promptQuestion = document.getElementById('promptQuestion');
+
+let currentColor = 'white';
+let lastInput;
+
+blackButton.addEventListener("click", function() {
+    currentColor = 'black';
+});
+
+greyButton.addEventListener("click", function() {
+    currentColor = 'grey';
+});
+
+rainbowButton.addEventListener("click", function() {
+    currentColor = 'rainbow';
+});
+
+eraserButton.addEventListener("click", function() {
+    currentColor = 'white';
+});
+
+clearButton.addEventListener("click", () => {
+    //Removing each row in the container
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    //Recreating the grid with the most recent grid size entered by user
+    createGrid(lastInput);
+});
+
+
 function createGrid(gridAmount) {
 
     //generates rows on the container
@@ -19,19 +57,37 @@ function createGrid(gridAmount) {
             column.style.height = `${500 / gridAmount}px`;
             //adding the columns within the row
             row.appendChild(column);
+            column.addEventListener("mouseover", function(){
+                if (currentColor === 'black') {
+                    column.style.backgroundColor = 'black';
+                } else if (currentColor === 'grey') {
+                    column.style.backgroundColor = 'grey';
+                } else if (currentColor === 'white') {
+                    column.style.backgroundColor = 'white';
+                } else if (currentColor === 'rainbow') {
+                    const rainbow = '#'+ Math.floor(Math.random()*16777215).toString(16);
+                    console.log('Generated Color:', rainbow);
+                    column.style.backgroundColor = rainbow;
+                }
+            });
         }
     }
 }
 
-//Getting user input
-var input = prompt("Please enter a grid size from 1-100");
-
-//Error checking
-while (input < 1 || input > 100) {
-    alert("Please enter a value within the range of 1-100");
+promptQuestion.addEventListener("click", function() {
+    //Getting user input
     var input = prompt("Please enter a grid size from 1-100");
-}
 
-//Calling the function if input is valid
-createGrid(input);
+    //Error checking
+    while (input < 1 || input > 100) {
+        alert("Please enter a value within the range of 1-100");
+        var input = prompt("Please enter a grid size from 1-100");
+    }
+    
+    //storing the final valid input value
+    lastInput = input;
+
+    //Calling the function if input is valid
+    createGrid(input);
+});
 
